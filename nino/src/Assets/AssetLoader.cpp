e38063e2d18e029f7dfb4e24a63b3415be25ee0c
Matheus Marchetti <matheusrmarchetti@gmail.com>
2022-07-26 23:08:24 -0300

    Added profiling measurement to AssetLoader

--- nino/src/Assets/AssetLoader.cpp
+++ nino/src/Assets/AssetLoader.cpp
@@ -1,6 +1,8 @@
 #include "corepch.h"
 #include "AssetLoader.h"
 
+#include "Core/Timestep.h"
+
 namespace nino
 {
 	bool AssetLoader::HasAssetBinary(uint64_t assetID)
@@ -16,26 +18,38 @@ namespace nino
 
 		std::filesystem::path texturePath = filePath;
 
+		Timestep time;
+
 		if (texturePath.extension() == ".dds")
 		{
+			time.Tick();
 			ThrowOnError(LoadFromDDSFile(texturePath.c_str(), DDS_FLAGS_NONE, nullptr, loadedImage));
+			time.Tick();
 		}
 		else if (texturePath.extension() == ".tga")
 		{
+			time.Tick();
 			ThrowOnError(LoadFromTGAFile(texturePath.c_str(), nullptr, loadedImage));
+			time.Tick();
 		}
 		else if (texturePath.extension() == ".hdr")
 		{
+			time.Tick();
 			ThrowOnError(LoadFromHDRFile(texturePath.c_str(), nullptr, loadedImage));
+			time.Tick();
 		}
 		else if (texturePath.extension() == ".bmp" || texturePath.extension() == ".jpg" || texturePath.extension() == ".png" || texturePath.extension() == ".tiff" || texturePath.extension() == ".gif")
 		{
+			time.Tick();
 			ThrowOnError(LoadFromWICFile(texturePath.c_str(), WIC_FLAGS_NONE, nullptr, loadedImage));
+			time.Tick();
 		}
 		else
 		{
 			throw std::exception("Unsupported texture format!");
 		}
+
+		NINO_CORE_INFO("Loading of {} image of dimensions {} x {} took {} ms", texturePath.extension().string(), loadedImage.GetMetadata().width, loadedImage.GetMetadata().height, time);
 
 		return loadedImage;
 	}
